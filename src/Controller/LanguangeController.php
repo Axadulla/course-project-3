60ba9957c65ab0a67fde48ac9a4a03a7fdd4eb22
Axadulla <ahadullaa5@gmail.com>
2025-06-28 16:17:31 +0500

    Add language switch controller

---
+++ src/Controller/LanguangeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+final class LanguangeController extends AbstractController
+{
+
+    #[Route('/switch-language', name: 'app_switch_language', methods: ['POST'])]
+    public function switchLanguage(Request $request): RedirectResponse
+    {
+        $session = $request->getSession();
+        $current = $session->get('_locale', 'en');
+        $session->set('_locale', $current === 'en' ? 'ru' : 'en');
+
+        return $this->redirect($request->headers->get('referer') ?? '/');
+    }
+}
