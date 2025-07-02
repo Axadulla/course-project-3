2c30994f7b8472a97c104cb7ff7807cd25dae31b
Axadulla <ahadullaa5@gmail.com>
2025-07-02 15:49:37 +0500

    Add CommentType form for comment submission

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -2,7 +2,30 @@
 
 namespace App\Form;
 
-class CommentType
-{
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'label' => 'Комментарий',
+                'attr' => [
+                    'rows' => 4,
+                    'class' => 'w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-500'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
 }
